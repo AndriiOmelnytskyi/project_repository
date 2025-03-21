a22fad1c6a9d926cef556f228e9c018f8d367e4d
AkTIyK123!Q <andrii.omelnytskyi@nure.ua>
2025-03-21 18:14:54 +0200

    added information about Token and Key

--- src/main/java/ZephyrUploader.java
+++ src/main/java/ZephyrUploader.java
@@ -13,9 +13,10 @@ public class ZephyrUploader {
     /** Клас для завантаження звітів до Zephyr scale після пробігу автоматизованих тестів */
     public static void main(String[] args) {
 
-        String TokenOfYourZephyrScaleAPI = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjb250ZXh0Ijp7ImJhc2VVcmwiOiJodHRwczovL2FuZHJpaW9tZWxueXRza3lpLTE3NDI0MDM3NTE3MTguYXRsYXNzaWFuLm5ldCIsInVzZXIiOnsiYWNjb3VudElkIjoiNzEyMDIwOjA0MjZhYTMwLTRkMmYtNGYwMy04ZjIyLWE0NTVlMTY2MjNmMiIsInRva2VuSWQiOiJmODgzMjU4OS1mMGViLTRiZGItOTYwNi01MmQ2MmU1MmZlNDMifX0sImlzcyI6ImNvbS5rYW5vYWgudGVzdC1tYW5hZ2VyIiwic3ViIjoiMzBhNTUxMjMtZjM4YS0zNDA1LThlNzItNjUzMmU1YTE5Yzc4IiwiZXhwIjoxNzczOTQwMDA0LCJpYXQiOjE3NDI0MDQwMDR9.1Cb21gQ3Bdy0ZA2piyz5IhhHexeCOylADxQhZpoWAao";
+        /* Для використання даного класу слід додати Token ZephyrScale та Ключ вашого проекту в Jira */
+        String TokenOfYourZephyrScaleAPI = "TokenOfYourZephyrScaleAPI";
         File reportFile = new File("test-output/MyTestSuite/mainPageTestsBuild.xml");
-        String keyOfYourProject = "SMS";
+        String keyOfYourProject = "keyOfYourProject";
 
         /* URL Zephyr Scale */
         String zephyrApiUrl = "https://api.zephyrscale.smartbear.com/v2/automations/executions/junit";
